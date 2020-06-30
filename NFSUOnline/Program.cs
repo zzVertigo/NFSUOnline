b174112f26b8442cc6c15ba8f568c2079abc2393
zzVertigo <alt.gk-1mh10h2@yopmail.com>
2020-06-29 21:06:14 -0700

    base thing

---
+++ NFSUOnline/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace NFSUOnline
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Thread.Sleep(-1);
+        }
+    }
+}
